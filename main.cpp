7d8561f98922ca424df6b4c527caf7156b330b73
Sergey <snam53.4e@gmail.com>
2021-10-13 02:25:33 -0400

    add

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
-
-#include "funcs.h"
+#include "vigenere.h"
+#include "caesar.h"
+#include "decrypt.h"
 
 int main()
 {
+  std::cout<<"encryptCaesar('Hello, World!', 10) and encryptCaesar('Way to Go!', 5)\n";
+  std::cout<<encryptCaesar("Hello, World!", 10)<<"\n";
+  std::cout<<encryptCaesar("Way to Go!", 5)<<"\n";
+  std::cout<<"encryptVigenere('Hello, World!', 'cake') and encryptVigenere('LMNOPQRS', 'b')\n";
+  std::cout<<encryptVigenere("Hello, World!", "cake")<<"\n";
+  std::cout<<encryptVigenere("LMNOPQRS", "b")<<"\n";
+  std::cout<<"decryptCaesar('Rovvy,Gybven!',10) and ('ABC, ABC',1)\n";
+  std::cout<<decryptCaesar("Rovvy, Gybvn!",10)<<"\n";
+  std::cout<<decryptCaesar("ABC, ABC",1)<<"\n";
+  std::cout<<"decryptVigenere('Jevpq, Wyvnd!','cake')\n";
+  std::cout<<decryptVigenere("Jevpq, Wyvnd!","cake");
   return 0;
 }
